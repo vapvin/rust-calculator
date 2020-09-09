5d4dd034c1a6197bba8ea82b9fab517273e1381f
vapvin <tpehdj12@daum.net>
2020-09-09 21:39:13 +0900

    Return Func list

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,17 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
+static mut input_val: f64 = 0.0;
+static mut total_val: f64 = 0.0;
+static mut this_method: i32 = 0;
+
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, rust-calculator!");
+pub fn return_input_val() -> String {
+    unsafe {
+        format!("{}", input_val).into()
+    }
 }
