96ed9e123548a68297abdd789afd87c8268ede30
vapvin <tpehdj12@daum.net>
2020-09-09 21:48:14 +0900

    Equal and clear, clicked button method

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,48 @@ pub fn return_input_val() -> String {
         format!("{}", input_val).into()
     }
 }
+
+#[wasm_bindgen]
+pub fn return_total_val() -> String {
+    unsafe {
+        format!("{}", total_val).into()
+    }
+}
+
+#[wasm_bindgen]
+pub fn change_input_val(stringval: &str) {
+    let val = stringval.parse::<f64>().unwrap();
+
+    unsafe {
+        input_val = input_val.mul_add(10.0, val);
+    }
+}
+
+#[wasm_bindgen]
+pub fn clear() {
+    unsafe {
+        input_val = 0.0;
+    }
+}
+
+#[wasm_bindgen]
+pub fn all_clear() {
+    unsafe {
+        input_val = 0.0;
+        total_val = 0.0;
+    }
+}
+
+#[wasm_bindgen]
+pub fn equal() {
+    unsafe {
+        input_val = total_val;
+    }
+}
+
+#[wasm_bindgen]
+pub fn chage_sig() {
+    unsafe {
+        input_val = -(input_val);
+    }
+}
